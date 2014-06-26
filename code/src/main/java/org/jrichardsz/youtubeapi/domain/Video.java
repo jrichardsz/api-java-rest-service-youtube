53e780f82a53185854b9dc6ad7d190768bb4599a
jrichardsz <import.linet@gmail.com>
2014-06-26 00:36:40 -0500

    usage jsoup for query dom

---
+++ code/src/main/java/org/jrichardsz/youtubeapi/domain/Video.java
@@ -0,0 +1,26 @@
+package org.jrichardsz.youtubeapi.domain;
+
+public class Video {
+
+	public String thumbnailUrlImage;
+	public String videoTitle;
+
+	public String getThumbnailUrlImage() {
+		return thumbnailUrlImage;
+	}
+
+	public void setThumbnailUrlImage(String thumbnailUrlImage) {
+		this.thumbnailUrlImage = thumbnailUrlImage;
+	}
+
+	public String getVideoTitle() {
+		return videoTitle;
+	}
+
+	public void setVideoTitle(String videoTitle) {
+		this.videoTitle = videoTitle;
+	}
+
+
+
+}
---
+++ code/src/test/java/org/jrichardsz/youtubeapi/rest/test/TestJSoup.java
@@ -0,0 +1,35 @@
+package org.jrichardsz.youtubeapi.rest.test;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class TestJSoup {
+	public static void main(String[] args) throws IOException {
+		Document doc = Jsoup.connect("http://www.youtube.com/user/ZERL1NG/videos?view=0&flow=grid").get();
+		Elements newsHeadlines = doc.select("div.yt-lockup.clearfix.yt-lockup-video.yt-lockup-grid");
+		Iterator<Element> iterator = newsHeadlines.iterator(); 
+		
+		while(iterator.hasNext()){
+			//System.out.println("##############");
+			Element element = iterator.next();			
+//			System.out.println(element.html());
+			System.out.println(element.select("h3.yt-lockup-title a").attr("title"));
+			//System.out.println(element.select(".yt-thumb-clip").select("img").html());
+			
+			Iterator<Element> thumbnail = element.select(".yt-thumb-clip").iterator();
+			
+			while(thumbnail.hasNext()){
+				Element thumb = thumbnail.next();
+				System.out.println(thumb.getElementsByTag("img").attr("src"));
+			}
+			
+			//System.out.println(element.select(".yt-thumb-clip").get(0).getElementsByTag("img").html());
+		}
+		
+	}
+}
